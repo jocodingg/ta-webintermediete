2f50501aed72d2d348d1184c8578a7c45d87aab9
brooooooooooooooooooooooooooo <jokosty111@gmail.com>
2025-05-22 21:48:41 +0700

    Firs commit

---
+++ src/sw.js
@@ -0,0 +1,71 @@
+const CACHE_NAME = 'story-app-cache-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/public/app.manifest',
+  '/public/favicon.png',
+  '/public/images/icon-192x192.png',
+  '/public/images/icon-512x512.png',
+  '/public/images/logo.png',
+  '/styles/styles.css',
+  '/scripts/index.js',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const requestUrl = event.request.url;
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+        .catch(() => caches.match('/'));
+    })
+  );
+});
+
+self.addEventListener('push', function (event) {
+  let data = {};
+
+  try {
+    data = event.data.json();
+  } catch (e) {
+    data = {
+      title: 'Notifikasi',
+      options: { body: 'Push message received.' }
+    };
+  }
+
+  const { title, options } = data;
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/')
+  );
+});
